374a3fa6a67772b976852bc856c65a0d8453100d
h-e19 <158261631+h-e19@users.noreply.github.com>
2025-11-15 13:40:01 +0500

    feat:placeholder user dashboard

--- main.py
+++ main.py
@@ -15,6 +15,11 @@ class Tick(BaseModel):
 def read_root():
     return FileResponse("static/index.html")
 
+@app.get("/user_dashboard")
+def read_root():
+    return FileResponse("static/user_dashboard.html")
+
+
 @app.post("/")
 def process(tick: Tick):
     return {"message": f"This is processed tick: {tick.tick}"}
